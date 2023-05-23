c0bedd7bf08c74b2375a8b21b938b7aa65820928
Ihsanudin Al Hanif <ihsanudinalkhanif@gmail.com>
2023-05-23 19:33:48 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Ihsanudin Al Hanif'; // TODO: replace this
+
+const studentId = 'FE5499980'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace megi'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
